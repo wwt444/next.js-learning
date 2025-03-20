b18ae25b8809ec0c399c257a4bef5b584d60591e
wangwentao <wangwt@csdn.net>
2025-03-20 17:05:22 +0800

    fix: 添加eslint规则

--- dashboard/starter-example/.eslintrc.js
+++ dashboard/starter-example/.eslintrc.js
@@ -7,4 +7,12 @@ module.exports = {
       rootDir: ['basics/*/', 'dashboard/*/', 'seo/'],
     },
   },
+  rules: {
+    'next/no-html-link-for-pages': [
+      'error',
+      {
+        pagesDir: './app' // 与 next.config.js 中配置的路径一致
+      }
+    ]
+  }
 };
